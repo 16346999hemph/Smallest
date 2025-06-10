d700717842608784ca4d271985643b083d4ce941
16346999hemph <ahvdr@umsystem.edu>
2025-06-10 16:33:43 -0500

    Add files via upload

---
+++ smallest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d, minimum;
+	char again = 'Y';
+
+	while (again == 'Y') {
+		cout << "Enter 4 integers: ";
+		cin >> a >> b >> c >> d;
+		minimum = a;
+		if (b < minimum) {
+			minimum = b;
+		}
+		if (c < minimum) {
+			minimum = c;
+		}
+		if (d < minimum) {
+			minimum = d;
+		}
+		cout << "The smallest number is: " <<
+			minimum;
+		cout << " Again? ";
+		cin >> again;
+	}
+
+	return 0;
+}
